59e29f55a71c9aad934fe3726ea8bac2dded9c3b
saeed anvari <sadixus@gmail.com>
2022-11-14 00:39:01 +0330

    initial

---
+++ components/layout/Navbar.tsx
@@ -0,0 +1,8 @@
+export default function Navbar(){
+    return(
+        <div className="fixed flex bg-transparent top-0 py-2 w-full border-b border-gray-300 justify-between">
+                <div>home</div>
+                <div>contact</div>
+        </div>
+    )
+}
