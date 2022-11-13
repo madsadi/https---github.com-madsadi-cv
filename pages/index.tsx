148b3ad1593acde6fbbdf525ac31137a77417f18
saeed anvari <sadixus@gmail.com>
2022-11-14 00:49:24 +0330

    hihi

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <section className='relative bg-developer bg-cover h-[500px] bg-no-repeat bg-center'>
         <div className='absolute h-full w-full  backdrop-blur-sm' />
       </section>
-      <section className='relative h-[500px] bg-white'>
+      <section className='relative h-[500px] bg-white hi'>
         <div className={'rounded-full bg-[url(/profile.jpeg)] bg-cover bg-no-repeat overflow-hidden h-[200px] w-[200px] absolute -top-[100px] left-[50%] -translate-x-[50%] border-2 border-white'} />
       </section>
     </>
