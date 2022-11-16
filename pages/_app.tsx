d38c26fba15534e0fb087b97363e0c2cad01679c
saeed anvari <sadixus@gmail.com>
2022-11-16 18:11:21 +0330

    lot of changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Head>
+      <title>Saeed Anvari Resume</title>
+    </Head>
+      <Component {...pageProps} />
+    </>)
 }
